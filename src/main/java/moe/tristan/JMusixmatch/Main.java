96d0d002ca9026c29b75eaf4c6a010204bd61047
Tristan Deloche <tristandeloche@gmail.com>
2016-07-05 04:10:43 +0200

    initial commit

---
+++ src/main/java/moe/tristan/JMusixmatch/Main.java
@@ -0,0 +1,31 @@
+/*
+ *     <DESCRIPTION>
+ *     Copyright (C) 2016 Tristan Deloche
+ *
+ *     This program is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package moe.tristan.JMusixmatch;
+
+/**
+ * Created by Tristan Deloche on 05/07/2016.
+ */
+public class Main {
+    @Gettter
+    private static final WidgetCore INSTANCE = new WidgetCore();
+
+    public static void main(String... args) {
+        INSTANCE.run();
+    }
+}
